fdc218da01edefdb5decd93cda075714e16b3bf9
Priyanka Masiwal <priyankamasiwal26025@gmail.com>
2025-11-02 19:50:40 +0530

    Initial Commit 1.4

--- tests/test_model.py
+++ tests/test_model.py
@@ -7,7 +7,7 @@ class TestModelTraining(unittest.TestCase):
 	def test_model_training(self):
 		model = joblib.load('model/california_housing.pkl')
 		self.assertIsInstance(model, LinearRegression)
-		self.assertGreaterEqual(len(model.feature_importances_), 4)
+		self.assertGreaterEqual(len(model.coef_), 4)
       
 if __name__ == '__main__':
 	unittest.main()
